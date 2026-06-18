147f94a17e4ee154ad794a53efab76526cdeeb36
Abdulsamad Yusuf <abdulsamadolohuntosinyusuf@gmail.com>
2026-06-18 10:47:43 +0100

    Fix base URL for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/sual/',
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Sual - Islamic Knowledge',
-        short_name: 'Sual',
-        description: 'Ask questions on Fiqh, Seerah, Arabiyyah and more',
-        theme_color: '#094570',
-        background_color: '#F0F8FF',
-        display: 'standalone',
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-      }
-    })
-  ]
+  plugins: [react()]
 })
