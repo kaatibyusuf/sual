7741f7205815ffea4e8d359bdf260e2c5adda9af
Abdulsamad Yusuf <abdulsamadolohuntosinyusuf@gmail.com>
2026-07-04 21:29:14 +0100

    Fix broken Routes block causing white screen

--- src/App.jsx
+++ src/App.jsx
@@ -159,9 +159,7 @@ export default function App() {
         />
         <main className="main-content">
           <Routes>
-            <Routes>
-  <Route path="/" element={<Home user={user} />} />
-            <Route path="/disciplines" element={<Disciplines />} />
+            <Route path="/" element={<Home user={user} />} />
             <Route path="/discipline/:id" element={<Discipline userLevel={userLevel} />} />
             <Route path="/quiz" element={<Quiz user={user} userLevel={userLevel} />} />
             <Route path="/flashcards" element={<Flashcards />} />
@@ -173,8 +171,10 @@ export default function App() {
             <Route path="/dashboard" element={<Dashboard user={user} />} />
             <Route path="/spaces" element={<Spaces user={user} />} />
             <Route path="/prayer-times" element={<PrayerTimes />} />
+            <Route path="/disciplines" element={<Discipline />} />
             <Route path="*" element={<Home user={user} />} />
-</Routes>
+            <Route path="/disciplines" element={<Disciplines />} />
+<Route path="/discipline/:id" element={<Discipline userLevel={userLevel} />} />
           </Routes>
         </main>
       </div>
