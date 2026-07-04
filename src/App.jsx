fcadc4c8af88a7cbe4b9788fab7b8952c85633f6
Abdulsamad Yusuf <abdulsamadolohuntosinyusuf@gmail.com>
2026-07-04 20:51:13 +0100

    Fix discipline route

--- src/App.jsx
+++ src/App.jsx
@@ -170,6 +170,7 @@ export default function App() {
             <Route path="/dashboard" element={<Dashboard user={user} />} />
             <Route path="/spaces" element={<Spaces user={user} />} />
             <Route path="/prayer-times" element={<PrayerTimes />} />
+            <Route path="/disciplines" element={<Discipline />} />
             <Route path="*" element={<Home user={user} />} />
           </Routes>
         </main>
